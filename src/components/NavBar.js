74c96afefca26624bbb802db957c2a29644e0a48
Amen Atikpo <alaskale2024@gmail.com>
2022-07-19 02:48:02 +0000

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+
+  
+  
 
 function NavBar() {
   return (
-    <nav>
-      <a href="#home">I'm a link!</a>
+    <nav> 
+      <a href="./Home"> Home</a>
+      <a href="./About"> About</a>   
+      <Home/>
+      <About/>   
     </nav>
+    
+
   );
 }
+export default NavBar;
